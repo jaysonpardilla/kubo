a2991f9c59ee12e35b73fb9de3cdbf136487323e
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-11 12:46:26 +0800

    fix 4

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -4,9 +4,9 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from core.chat.routing import websocket_urlpatterns
+from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
