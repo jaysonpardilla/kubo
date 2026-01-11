11a3d0e001455f0d39acc184cf0fff51f707e9aa
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-11 11:49:54 +0800

    fix chat 3

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,14 +1,17 @@
+# core/asgi.py
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
+from core.chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+        URLRouter(websocket_urlpatterns)
     ),
 })
